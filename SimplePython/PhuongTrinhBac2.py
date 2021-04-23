6f80f853b09c6e02987402a5a018c5dc9c644100
ITLTVPo <57531319+ITLTVPo@users.noreply.github.com>
2021-04-23 17:45:43 +0700

    Added PhuongTrinhBac2.py

---
+++ SimplePython/PhuongTrinhBac2.py
@@ -0,0 +1,16 @@
+print("Chương trình giải phương trình bậc hai: ax**2 + bx + c = 0")
+try:
+    while(True):
+        a=int(input("a = "))
+        b=int(input("b = "))
+        c=int(input("c = "))
+        delta = b**2 - 4*a*c
+        if delta<0: 
+            print("delta = b**2 - 4*a*c = ",delta)
+            print("delta < 0 -> Phương trình vô nghiệm")
+        elif delta>=0:
+            print("delta = b**2 - 4*a*c = ",delta)
+            print("Phương trình có nghiệm x1 = ",(-b + delta ** (1/2))/(2*a))
+            print("Phương trình có nghiệm x2 = ",(-b - delta ** (1/2))/(2*a))
+except: 
+    print("Oops, something has gone wrong...")
